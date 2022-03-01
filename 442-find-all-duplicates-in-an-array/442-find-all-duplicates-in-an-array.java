05cbd3b5bb507c10efa222c48bd2bb214c9e07bd
Aniket Baidya <72697167+DEV-AB-03@users.noreply.github.com>
2022-03-01 11:07:56 +0530

    Time: 5 ms (95.26%), Space: 50.4 MB (81.00%) - LeetHub

--- 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.java
+++ 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.java
@@ -2,19 +2,18 @@ class Solution
 {
     public List<Integer> findDuplicates(int[] nums) 
     {
+        //SINCE WE HAVE NUMBERS BOUNDED BY ARRAY SIZE WE CAN TREAT THEM AS POSITIONS ALSO
+        //IF WE COME BACK TO THE SAME POSTION AGAIN AND WE FIND THE NUMBER ALREADY NEGATIVE THEN WE CAN SAY NUMBERS ARE REPEATED
         int n=nums.length;
         List<Integer>answer=new ArrayList<>();
-        int[]freq=new int[n+1];
-        for(int num:nums)
+        for(int i=0;i<n;i++)
         {
-            freq[num]++;
-        }
-        for(int i=0;i<n+1;i++)
-        {
-            if(freq[i]==2)
+            int pos=Math.abs(nums[i])-1;
+            if(nums[pos]<0)
             {
-                answer.add(i);
+                answer.add(Math.abs(nums[i]));
             }
+            nums[pos]=nums[pos]*(-1);
         }
         return answer;
     }
