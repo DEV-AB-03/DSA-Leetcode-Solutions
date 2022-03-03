dbce4d52d998b25d7edf60f5fc5428ca12d946fe
Aniket Baidya <72697167+DEV-AB-03@users.noreply.github.com>
2022-03-03 14:20:01 +0530

    Time: 1 ms, Memory: 39.4 MB - LeetHub

---
+++ sqrtx/sqrtx.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int mySqrt(int x) {
+        if (x==0) return 0;
+
+        int s=1,e = x;
+        //s= start, e = end
+        while(s<=e){
+		
+            int mid = s+(e-s)/2;
+			
+            if(mid > x/mid) e=mid-1;  //if mid^2 > x then decrease e to mid-1
+			
+            else if(mid<x/mid) s=mid+1; //if mid^2 <x then increase s to mid+1
+			
+            else return mid; //for perfect squares
+            
+        } 
+		// Here the trick is end will always be one less than start so we can return it in case of non perfect squares
+        return e;
+        
+    }
+}
