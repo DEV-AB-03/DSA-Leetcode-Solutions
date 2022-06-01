372cc3d8ccb0b1d607d7659fbf88d0df610652e4
Aniket Baidya <72697167+DEV-AB-03@users.noreply.github.com>
2022-06-01 07:53:17 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (52.75%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -0,0 +1,20 @@
+class Solution 
+{
+    public int climbStairs(int n) 
+    {
+        //Similar to fibonacci number
+        if(n<=1)
+        {
+            return n;
+        }
+        int steps=0;
+        int[]dp=new int[n+1];
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3;i<=n;i++)
+        {
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+}
