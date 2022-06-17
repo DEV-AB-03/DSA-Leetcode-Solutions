905fb0a294da9c418619b384a31727b706d79875
Aniket Baidya <72697167+DEV-AB-03@users.noreply.github.com>
2022-06-17 09:25:18 +0530

    Time: 2 ms (20.45%), Space: 42 MB (87.28%) - LeetHub

--- 968-binary-tree-cameras/968-binary-tree-cameras.java
+++ 968-binary-tree-cameras/968-binary-tree-cameras.java
@@ -1,40 +1,29 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution 
-{
+class Solution {
+    int ans;
+    Set<TreeNode> covered;
     public int minCameraCover(TreeNode root) {
-	var covered = new HashSet<TreeNode>();
-	covered.add(null);
-	return install(null, root, covered);
-}
+        ans = 0;
+        covered = new HashSet();
+        covered.add(null);
 
-private int install(TreeNode parent, TreeNode node, Set<TreeNode> covered) {
-	if (node == null)
-		return 0;
+        dfs(root, null);
+        return ans;
+    }
 
-	var count = install(node, node.left, covered) + 
-				install(node, node.right, covered);
+    public void dfs(TreeNode node, TreeNode par) {
+        if (node != null) {
+            dfs(node.left, node);
+            dfs(node.right, node);
 
-	if (parent == null && !covered.contains(node) || !covered.contains(node.left) || !covered.contains(node.right)) {
-		count++;
-		covered.add(node);
-		covered.add(parent);
-		covered.add(node.left);
-		covered.add(node.right);
-	}
-	return count;
-}
+            if (par == null && !covered.contains(node) ||
+                    !covered.contains(node.left) ||
+                    !covered.contains(node.right)) {
+                ans++;
+                covered.add(node);
+                covered.add(par);
+                covered.add(node.left);
+                covered.add(node.right);
+            }
+        }
+    }
 }
