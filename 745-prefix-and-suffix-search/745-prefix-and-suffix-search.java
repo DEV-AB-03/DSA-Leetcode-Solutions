4d73cc9596938bbb45faac60b26b213b03c492c3
Aniket Baidya <72697167+DEV-AB-03@users.noreply.github.com>
2022-06-18 08:24:01 +0530

    Time: 875 ms (18.38%), Space: 297.8 MB (5.00%) - LeetHub

--- 745-prefix-and-suffix-search/745-prefix-and-suffix-search.java
+++ 745-prefix-and-suffix-search/745-prefix-and-suffix-search.java
@@ -1,64 +1,21 @@
 class WordFilter {
-    TrieNode trie;
+  HashMap<String,Integer> map;
     public WordFilter(String[] words) {
-        trie = new TrieNode();
-        int wt = 0;
-        for (String word: words) {
-            TrieNode cur = trie;
-            cur.weight = wt;
-            int L = word.length();
-            char[] chars = word.toCharArray();
-            for (int i = 0; i < L; ++i) {
-
-                TrieNode tmp = cur;
-                for (int j = i; j < L; ++j) {
-                    int code = (chars[j] - '`') * 27;
-                    if (tmp.children.get(code) == null)
-                        tmp.children.put(code, new TrieNode());
-                    tmp = tmp.children.get(code);
-                    tmp.weight = wt;
+        map=new HashMap<>();
+        for(int idx=0;idx<words.length;idx++){
+            for(int i=0;i<words[idx].length();i++){
+                String pre=words[idx].substring(0,i+1);
+                for(int j=words[idx].length()-1;j>=0;j--){
+                    String suf=words[idx].substring(j);
+                    String s=pre+"#"+suf;
+                    map.put(s,idx);
                 }
-
-                tmp = cur;
-                for (int k = L - 1 - i; k >= 0; --k) {
-                    int code = (chars[k] - '`');
-                    if (tmp.children.get(code) == null)
-                        tmp.children.put(code, new TrieNode());
-                    tmp = tmp.children.get(code);
-                    tmp.weight = wt;
-                }
-
-                int code = (chars[i] - '`') * 27 + (chars[L - 1 - i] - '`');
-                if (cur.children.get(code) == null)
-                    cur.children.put(code, new TrieNode());
-                cur = cur.children.get(code);
-                cur.weight = wt;
-
             }
-            wt++;
         }
     }
-
+    
     public int f(String prefix, String suffix) {
-        TrieNode cur = trie;
-        int i = 0, j = suffix.length() - 1;
-        while (i < prefix.length() || j >= 0) {
-            char c1 = i < prefix.length() ? prefix.charAt(i) : '`';
-            char c2 = j >= 0 ? suffix.charAt(j) : '`';
-            int code = (c1 - '`') * 27 + (c2 - '`');
-            cur = cur.children.get(code);
-            if (cur == null) return -1;
-            i++; j--;
-        }
-        return cur.weight;
-    }
-}
-
-class TrieNode {
-    Map<Integer, TrieNode> children;
-    int weight;
-    public TrieNode() {
-        children = new HashMap();
-        weight = 0;
+        String s=prefix+"#"+suffix;
+        return map.getOrDefault(s,-1);
     }
 }
