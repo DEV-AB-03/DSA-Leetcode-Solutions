2327a4766e4cb520012f5f2e1829bc489a6a203e
Aniket Baidya <72697167+DEV-AB-03@users.noreply.github.com>
2022-06-29 12:18:21 +0530

    Time: 1 ms (92.58%), Space: 42.7 MB (13.79%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.java
+++ 392-is-subsequence/392-is-subsequence.java
@@ -2,24 +2,30 @@ class Solution
 {
     public boolean isSubsequence(String s, String t) 
     {
-        //2 POINTER APPROACH
+        //Map s to t
+        //2 pointer approach
         int l1=s.length();
         int l2=t.length();
-        int j=0;
-        int i=0;
-        while(i<l2 && j<l1)
+        
+        //Declare Pointers
+        int spointer=0;
+        int tpointer=0;
+        
+        while(spointer<l1 && tpointer<l2)
         {
-            if(s.charAt(j)==t.charAt(i))
+            char ch1=s.charAt(spointer);
+            char ch2=t.charAt(tpointer);
+            if(ch1==ch2)
             {
-                j++;
-                i++;
+                spointer++;
+                tpointer++;
             }
             else
             {
-                i++;
+                tpointer++;
             }
         }
-        if(j==l1)
+        if(spointer==l1)
         {
             return true;
         }
