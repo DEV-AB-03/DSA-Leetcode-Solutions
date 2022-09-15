068b4989451847c21c5748382ce6063369cce6e6
Aniket Baidya <72697167+DEV-AB-03@users.noreply.github.com>
2022-09-15 11:19:57 +0530

    Time: 58 ms (87.87%), Space: 135.3 MB (50.30%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.java
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.java
@@ -1,62 +1,40 @@
 class Solution 
 {
-    public void decrementinMap(Map<Integer,Integer>map,int key)
-    {
-        if(map.get(key)==1)
-        {
-            map.remove(key);
-        }
-        else
-        {
-            map.put(key,map.get(key)-1);
-        }
-    }
     public int[] findOriginalArray(int[] changed) 
     {
-        if(changed.length%2!=0)
+        //Count Sort Approach
+        int n=changed.length;
+        if(n%2!=0)
         {
             return new int[]{};
         }
-        //Hashing Approach
-        //So the idea is that if we find a pair such that element and its double exist in the aray then we remove it.
+        int[]count=new int[100001];
+        for(int i:changed)
+        {
+            count[i]++;
+        }
         
-        //We keep on doing so such that there exists no element in the array
-        //Then we return the array is such case is achieved
-        //Else even if one element exists in the array then we return empty array as it doesnt match the conditions
-        
-        //So first we sort the array so that we can get the smallest elements first
-        //Smallest elements are the ones whose halves wont exist in array as they are the first ones in array
-        
-        //Now sort the array
-        Arrays.sort(changed);
-        HashMap<Integer,Integer>map=new HashMap<>();
-        int[]originalArray=new int[changed.length/2];
+        int[]answer=new int[n/2];
         int idx=0;
-        //Now iterate over the array to satisfy the conditions
-        for(int ele:changed)
+        //Now iterate over the array
+        for(int i=0;i<100001;i++)
         {
-            if(ele%2==0 && map.containsKey(ele/2))
+            while(count[i]>0 && i*2<count.length && count[i*2]>0)
             {
-                decrementinMap(map,ele/2);
-                originalArray[idx]=ele/2;
+                count[i]--;
+                count[i*2]--;
+                answer[idx]=i;
                 idx++;
             }
-            else if(map.containsKey(2*ele))
-            {
-                decrementinMap(map,2*ele);
-                originalArray[idx]=ele;
-                idx++;
-            }
-            else
-            {
-                map.put(ele,map.getOrDefault(ele,0)+1);
-            }
         }
-        if(idx==originalArray.length)
+        for(int i=0;i<100001;i++)
         {
-            return originalArray;
+            if(count[i]!=0)
+            {
+                return new int[]{};
+            }
         }
-        return new int[]{};
+        return answer;
         
     }
 }
