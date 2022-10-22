2a12a65974ecce6ceb5768534fc969fe6b977c4c
Aniket Baidya <72697167+DEV-AB-03@users.noreply.github.com>
2022-10-22 09:17:57 +0530

    Time: 30 ms (42.24%), Space: 47.3 MB (36.01%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,72 +1,54 @@
 class Solution {
     public String minWindow(String s, String t) {
-
-        if (s.length() == 0 || t.length() == 0) {
+        
+        if (t.length() > s.length() || t.equals(""))
             return "";
+        
+        Map<Character, Integer> tMap = new HashMap<>();
+        Map<Character, Integer> sMap = new HashMap<>();
+        
+        for(char c : t.toCharArray()){
+            tMap.put(c, tMap.getOrDefault(c, 0) + 1);
+            sMap.put(c, 0);
         }
-
-        // Dictionary which keeps a count of all the unique characters in t.
-        Map<Character, Integer> dictT = new HashMap<Character, Integer>();
-        for (int i = 0; i < t.length(); i++) {
-            int count = dictT.getOrDefault(t.charAt(i), 0);
-            dictT.put(t.charAt(i), count + 1);
-        }
-
-        // Number of unique characters in t, which need to be present in the desired window.
-        int required = dictT.size();
-
-        // Left and Right pointer
-        int l = 0, r = 0;
-
-        // formed is used to keep track of how many unique characters in t
-        // are present in the current window in its desired frequency.
-        // e.g. if t is "AABC" then the window must have two A's, one B and one C.
-        // Thus formed would be = 3 when all these conditions are met.
-        int formed = 0;
-
-        // Dictionary which keeps a count of all the unique characters in the current window.
-        Map<Character, Integer> windowCounts = new HashMap<Character, Integer>();
-
-        // ans list of the form (window length, left, right)
-        int[] ans = { -1, 0, 0 };
-
-        while (r < s.length()) {
-            // Add one character from the right to the window
-            char c = s.charAt(r);
-            int count = windowCounts.getOrDefault(c, 0);
-            windowCounts.put(c, count + 1);
-
-            // If the frequency of the current character added equals to the
-            // desired count in t then increment the formed count by 1.
-            if (dictT.containsKey(c) && windowCounts.get(c).intValue() == dictT.get(c).intValue()) {
-                formed++;
+        
+        int start=0, matched=0;
+        int minLength = s.length();
+        int[] result = new int[]{-1,-1};
+		
+        for (int end=0; end < s.length(); end++){
+            char curr = s.charAt(end);
+            
+            // Check if current char is present in target, then update character count in source map
+            if(tMap.containsKey(curr)){
+                sMap.put(curr, sMap.get(curr) + 1);            
+                if(sMap.get(curr).equals(tMap.get(curr)))
+                    matched++;
             }
-
-            // Try and contract the window till the point where it ceases to be 'desirable'.
-            while (l <= r && formed == required) {
-                c = s.charAt(l);
-                // Save the smallest window until now.
-                if (ans[0] == -1 || r - l + 1 < ans[0]) {
-                    ans[0] = r - l + 1;
-                    ans[1] = l;
-                    ans[2] = r;
+            
+            // When a valid window is found, try to reduce it until a valid window is not found
+            if(matched == tMap.size()){
+                // Iterate until the window is not valid
+                while(matched == tMap.size()){
+                    char first = s.charAt(start++);
+                    // If current char is a character in target, 
+                    // then reduce its count from the sMap as we'll reduce its count
+                    if(sMap.containsKey(first)){
+                        sMap.put(first, sMap.get(first) - 1);
+                        // If count of first char in window < count in tMap, then reduce the matched count
+                        if(sMap.get(first) < tMap.get(first))
+                            matched--;
+                    }
                 }
-
-                // The character at the position pointed by the
-                // `Left` pointer is no longer a part of the window.
-                windowCounts.put(c, windowCounts.get(c) - 1);
-                if (dictT.containsKey(c) && windowCounts.get(c).intValue() < dictT.get(c).intValue()) {
-                    formed--;
+                // Update length if current length is smaller than previous
+                // Note - Check 1 character before start index as the start idx was already incremented in the while loop
+                if (end - (start - 1) < minLength){
+                    minLength = end - (start - 1) + 1;
+                    result[0] = start - 1;
+                    result[1] = end;
                 }
-
-                // Move the left pointer ahead, this would help to look for a new window.
-                l++;
             }
-
-            // Keep expanding the window once we are done contracting.
-            r++;
         }
-
-        return ans[0] == -1 ? "" : s.substring(ans[1], ans[2] + 1);
+        return result[0] != -1 ? s.substring(result[0], result[1] + 1) : "";
     }
 }
