033320d1e32e516e226ffd4b460c05c6067c7141
Aniket Baidya <72697167+DEV-AB-03@users.noreply.github.com>
2025-05-10 12:43:18 +0530

    Time: 0 ms (100%), Space: 42.6 MB (32.2%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -1,30 +1,32 @@
-class Solution 
-{
-    public int findPeakElement(int[] nums) 
-    {
-        int n=nums.length;
-        int ans=0;
-        if(n==0 || n==1)
-        {
+class Solution {
+    public int findPeakElement(int[] nums) {
+        // Array has only 1 element
+        if (nums.length == 1 || nums.length == 0) {
             return 0;
         }
-        if(nums[0]>nums[1])
-        {
+        // Check for First Element
+        if (nums[0] > nums[1]) {
             return 0;
         }
-        if(nums[n-1]>nums[n-2])
-        {
-            return n-1;
+        if (nums[nums.length - 1] > nums[nums.length - 2]) {
+            return nums.length - 1;
         }
-        for(int i=1;i<n-1;i++)
-        {
-            if(nums[i]>nums[i-1] && nums[i]>nums[i+1])
-            {
-               ans=i;
-                break;
+
+        // Now Perform Binary Search
+        int low = 1;
+        int high = nums.length - 2;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid - 1] < nums[mid] && nums[mid] > nums[mid + 1]) {
+                return mid;
+            } else if (nums[mid] > nums[mid - 1]) {
+                // Mid is on an increasing curve. So peak lies to right of mid
+                low = mid + 1;
+            } else {
+                // Mid is on an decreasing curve. So peak lies to left of mid
+                high = mid - 1;
             }
         }
-        return ans;
-        
+        return -1;
     }
 }
