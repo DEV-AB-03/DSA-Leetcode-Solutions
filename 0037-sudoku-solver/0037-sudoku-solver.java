dc8ac190491d5a83e5b28313b4320f1bfe9725eb
Aniket Baidya <72697167+DEV-AB-03@users.noreply.github.com>
2025-07-13 17:13:01 +0530

    Time: 383 ms (7.88%), Space: 41.2 MB (44.42%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -0,0 +1,41 @@
+class Solution {
+    public boolean isValidBoard(char[][] board, int row, int col, char c) {
+        for (int i = 0; i < 9; i++) {
+            if (board[i][col] == c)
+                return false;
+
+            if (board[row][i] == c)
+                return false;
+
+            if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c)
+                return false;
+        }
+        return true;
+    }
+
+    public boolean solve(char[][] board) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') {
+                    // We found an Empty Cell
+                    for (char c = '1'; c <= '9'; c++) {
+                        if (isValidBoard(board, i, j, c)) {
+                            board[i][j] = c;
+                            if (solve(board)) {
+                                return true;
+                            } else {
+                                board[i][j] = '.';
+                            }
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+}
