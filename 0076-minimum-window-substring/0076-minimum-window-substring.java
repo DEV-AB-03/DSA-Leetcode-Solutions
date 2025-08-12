e752ca4b42dfaebc6d305e47be080c668fc72979
Aniket Baidya <72697167+DEV-AB-03@users.noreply.github.com>
2025-08-12 20:10:35 +0530

    Time: 17 ms (63.04%), Space: 45 MB (60.97%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,54 +1,45 @@
 class Solution {
     public String minWindow(String s, String t) {
-        
-        if (t.length() > s.length() || t.equals(""))
+        if (s.length() < t.length())
             return "";
-        
-        Map<Character, Integer> tMap = new HashMap<>();
-        Map<Character, Integer> sMap = new HashMap<>();
-        
-        for(char c : t.toCharArray()){
-            tMap.put(c, tMap.getOrDefault(c, 0) + 1);
-            sMap.put(c, 0);
+
+        Map<Character, Integer> map = new HashMap<>();
+        for (char ch : t.toCharArray()) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
         }
-        
-        int start=0, matched=0;
-        int minLength = s.length();
-        int[] result = new int[]{-1,-1};
-		
-        for (int end=0; end < s.length(); end++){
-            char curr = s.charAt(end);
-            
-            // Check if current char is present in target, then update character count in source map
-            if(tMap.containsKey(curr)){
-                sMap.put(curr, sMap.get(curr) + 1);            
-                if(sMap.get(curr).equals(tMap.get(curr)))
-                    matched++;
+
+        int left = 0, right = 0;
+        int count = 0;
+        int minLen = Integer.MAX_VALUE;
+        int sIdx = -1;
+
+        while (right < s.length()) {
+            char chRight = s.charAt(right);
+            if (map.containsKey(chRight)) {
+                map.put(chRight, map.get(chRight) - 1);
+                if (map.get(chRight) >= 0) { // valid match
+                    count++;
+                }
             }
-            
-            // When a valid window is found, try to reduce it until a valid window is not found
-            if(matched == tMap.size()){
-                // Iterate until the window is not valid
-                while(matched == tMap.size()){
-                    char first = s.charAt(start++);
-                    // If current char is a character in target, 
-                    // then reduce its count from the sMap as we'll reduce its count
-                    if(sMap.containsKey(first)){
-                        sMap.put(first, sMap.get(first) - 1);
-                        // If count of first char in window < count in tMap, then reduce the matched count
-                        if(sMap.get(first) < tMap.get(first))
-                            matched--;
+
+            while (count == t.length()) { // all chars matched
+                if (right - left + 1 < minLen) {
+                    minLen = right - left + 1;
+                    sIdx = left;
+                }
+
+                char chLeft = s.charAt(left);
+                if (map.containsKey(chLeft)) {
+                    map.put(chLeft, map.get(chLeft) + 1);
+                    if (map.get(chLeft) > 0) { // losing a needed char
+                        count--;
                     }
                 }
-                // Update length if current length is smaller than previous
-                // Note - Check 1 character before start index as the start idx was already incremented in the while loop
-                if (end - (start - 1) < minLength){
-                    minLength = end - (start - 1) + 1;
-                    result[0] = start - 1;
-                    result[1] = end;
-                }
+                left++;
             }
+            right++;
         }
-        return result[0] != -1 ? s.substring(result[0], result[1] + 1) : "";
+
+        return sIdx == -1 ? "" : s.substring(sIdx, sIdx + minLen);
     }
 }
