bc7139b4269cb5e6707eba0df2fb12365612a436
Aniket Baidya <72697167+DEV-AB-03@users.noreply.github.com>
2025-09-26 15:11:31 +0100

    Time: 634 ms (14.22%), Space: 41 MB (97.75%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,22 +1,21 @@
 class Solution {
-    int count = 0;
+    public int dfs(int[] nums, int index, int sum, int target) {
+        if (index == nums.length) {
+            if (sum == target) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+        //Explore All Possible paths
+        int addPlusSign = dfs(nums, index + 1, sum + nums[index], target);
+        int addMinusSign = dfs(nums, index + 1, sum - nums[index], target);
 
-    public int findTargetSumWays(int[] nums, int target) {
-        backtrack(nums, 0, 0, target);
-        return count;
+        return addPlusSign + addMinusSign;
     }
 
-    private void backtrack(int[] nums, int index, int currentSum, int target) {
-        if (index == nums.length) {
-            if (currentSum == target) {
-                count++;
-            }
-            return;
-        }
-
-        // Choose '+'
-        backtrack(nums, index + 1, currentSum + nums[index], target);
-        // Choose '-'
-        backtrack(nums, index + 1, currentSum - nums[index], target);
+    public int findTargetSumWays(int[] nums, int target) {
+        // Recursive Solution
+        return dfs(nums, 0, 0, target);
     }
 }
