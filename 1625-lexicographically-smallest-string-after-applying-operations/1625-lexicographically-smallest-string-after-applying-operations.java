1b2d9c67fc584ff47317e113acce781b32a5a7a5
Aniket Baidya <72697167+DEV-AB-03@users.noreply.github.com>
2025-10-20 17:13:32 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1625-lexicographically-smallest-string-after-applying-operations/1625-lexicographically-smallest-string-after-applying-operations.java
+++ 1625-lexicographically-smallest-string-after-applying-operations/1625-lexicographically-smallest-string-after-applying-operations.java
@@ -1,37 +1,44 @@
-import java.util.*;
-
 class Solution {
-    public String findLexSmallestString(String s, int a, int b) {
-        Set<String> visited = new HashSet<>();
-        String smallest = s;
-        int n = s.length();
-        Queue<String> q = new LinkedList<>();
-        q.offer(s);
-        visited.add(s);
+    String smallest = "";
 
-        while (!q.isEmpty()) {
-            String cur = q.poll();
-            if (cur.compareTo(smallest) < 0) {
-                smallest = cur;
-            }
-
-            // Operation 1: add 'a' to all odd indices
-            char[] arr = cur.toCharArray();
-            for (int i = 1; i < n; i += 2) {
-                arr[i] = (char) ((arr[i] - '0' + a) % 10 + '0');
-            }
-            String added = new String(arr);
-            if (visited.add(added)) {
-                q.offer(added);
-            }
-
-            // Operation 2: rotate right by b
-            String rotated = cur.substring(n - b) + cur.substring(0, n - b);
-            if (visited.add(rotated)) {
-                q.offer(rotated);
-            }
+    public String addAtoS(String s, int a) {
+        char[] arr = s.toCharArray();
+        for (int i = 1; i < arr.length; i += 2) {
+            int val = ((arr[i] - '0') + a) % 10;
+            arr[i] = (char) ('0' + val);
         }
+        return new String(arr);
+    }
 
+    public String rotateByB(String s, int b) {
+        int n = s.length();
+        b = b % n;
+        return s.substring(n - b) + s.substring(0, n - b);
+    }
+
+    public void dfs(String s, int a, int b, List<String> vis) {
+        if (vis.contains(s)) {
+            return;
+        }
+        vis.add(s);
+        // Check if current modified string is lexicographically smallest
+        // Update smallest string
+        if (smallest.isEmpty() || s.compareTo(smallest) < 0) {
+            smallest = s;
+        }
+        // Try out all Possible Cases
+        dfs(addAtoS(s, a), a, b, vis);
+
+        dfs(rotateByB(s, b), a, b, vis);
+
+    }
+
+    public String findLexSmallestString(String s, int a, int b) {
+        // DFS Approach
+        // This is essentially a graphs problem where each node is a string
+        List<String> vis = new ArrayList<>();
+        smallest = s;
+        dfs(s, a, b, vis);
         return smallest;
     }
 }
