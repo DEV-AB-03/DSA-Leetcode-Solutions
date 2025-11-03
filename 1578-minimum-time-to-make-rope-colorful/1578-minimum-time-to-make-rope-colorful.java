cb27c39774fc8116f2274f8dfd7e4bea0f45e254
Aniket Baidya <72697167+DEV-AB-03@users.noreply.github.com>
2025-11-03 10:16:37 +0000

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.java
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.java
@@ -1,26 +1,38 @@
 class Solution {
-    public int minCost(String colors, int[] neededTime) {
-        // Initalize two pointers i, j.
-        int totalTime = 0;
-        int i = 0, j = 0;
-        
-        while (i < neededTime.length && j < neededTime.length) {
-            int currTotal = 0, currMax = 0;
-            
-            // Find all the balloons having the same color as the 
-            // balloon indexed at i, record the total removal time 
-            // and the maximum removal time.
-            while (j < neededTime.length && colors.charAt(i) == colors.charAt(j)) {
-                currTotal += neededTime[j];
-                currMax = Math.max(currMax, neededTime[j]);
-                j++;
-            }
-            
-            // Once we reach the end of the current group, add the cost of 
-            // this group to total_time, and reset two pointers.
-            totalTime += currTotal - currMax;
-            i = j;
+    public int dfs(String s, int index, char prevColor, int prevColIndex, int[] neededTime, int[][] dp) {
+        // Base Case
+        if (index >= s.length()) {
+            return 0;
         }
-        return totalTime;    
+        if (dp[index][prevColIndex + 1] != -1) {
+            return dp[index][prevColIndex + 1];
+        }
+        int ans;
+        // Explore all Possible Paths
+        if (s.charAt(index) != prevColor) {
+            // These balloons are not same color. So we simply skip.
+            ans = dp[index][prevColIndex] = dfs(s, index + 1, s.charAt(index), index, neededTime, dp);
+        } else {
+            // Colors are same. So we can either remove previous or remove this
+            int removeFirst = neededTime[prevColIndex] + dfs(s, index + 1, s.charAt(index), index, neededTime, dp);
+            int removeCurrent = neededTime[index] + dfs(s, index + 1, prevColor, prevColIndex, neededTime, dp);
+
+            ans = Math.min(removeFirst, removeCurrent);
+        }
+        return dp[index][prevColIndex + 1] = ans;
+    }
+
+    public int minCost(String colors, int[] neededTime) {
+        // Recursion Solution
+        // Memoized Solution
+        if (colors.length() == 0)
+            return 0;
+        int[][] dp = new int[colors.length()][colors.length() + 1];
+        for (int i = 0; i < colors.length(); i++) {
+            Arrays.fill(dp[i], -1);
+        }
+        // Start recursion from index 1, with first balloon as previous
+        return dfs(colors, 1, colors.charAt(0), 0, neededTime, dp);
+
     }
 }
