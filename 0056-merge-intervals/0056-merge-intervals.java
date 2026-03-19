a2c7acb671206e95bdafa60190d64b09d8d33cb3
Aniket Baidya <aniketbaidyarocks@gmail.com>
2026-03-19 07:56:33 +0000

    Time: 12 ms (10.88%), Space: 49.2 MB (52.42%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,17 +1,20 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        // Sort According to begin time
-        Arrays.sort(intervals, (o1, o2) -> o1[0] - o2[0]);
-        LinkedList<int[]> merged = new LinkedList<>();
+        // Sorting
+        // Sort According to start time
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+
+        // List
+        List<int[]> list = new ArrayList<>();
         for (int[] interval : intervals) {
-            if (merged.isEmpty() || interval[0] > merged.getLast()[1]) {
-                // Simply insert this interval
-                merged.add(interval);
+            if (list.isEmpty() || list.get(list.size() - 1)[1] < interval[0]) {
+                // No overlap
+                list.add(interval);
             } else {
-                // We have a interval that sits between the interval in list
-                merged.getLast()[1] = Math.max(merged.getLast()[1], interval[1]);
+                // Overlap
+                list.get(list.size() - 1)[1] = Math.max(list.get(list.size() - 1)[1], interval[1]);
             }
         }
-        return merged.toArray(new int[merged.size()][]);
+        return list.toArray(new int[list.size()][]);
     }
 }
