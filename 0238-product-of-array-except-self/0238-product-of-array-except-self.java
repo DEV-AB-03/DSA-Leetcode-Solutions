75bcbb107b5516855e3240c58c22232fe05ae31a
Aniket Baidya <aniketbaidyarocks@gmail.com>
2026-04-09 10:19:35 +0100

    Time: 2 ms (95.1%), Space: 64.7 MB (91.03%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,22 +1,26 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        // Prefix Multiplication Approach
-        int[] left = new int[nums.length];
-        int[] right = new int[nums.length];
-        int leftProd = 1;
-        int rightProd = 1;
+        // Prefix and Suffix
+        int[] pre = new int[nums.length];
+        int[] suff = new int[nums.length];
+
+        int lp = 1;
+        int rp = 1;
         for (int i = 0; i < nums.length; i++) {
-            left[i] = leftProd;
-            leftProd *= nums[i];
+            pre[i] = lp;
+            lp *= nums[i];
         }
+
         for (int i = nums.length - 1; i >= 0; i--) {
-            right[i] = rightProd;
-            rightProd *= nums[i];
+            suff[i] = rp;
+            rp *= nums[i];
         }
+
         int[] ans = new int[nums.length];
-        for (int i = 0; i < ans.length; i++) {
-            ans[i] = left[i] * right[i];
+        for (int i = 0; i < nums.length; i++) {
+            ans[i] = pre[i] * suff[i];
         }
         return ans;
+
     }
 }
