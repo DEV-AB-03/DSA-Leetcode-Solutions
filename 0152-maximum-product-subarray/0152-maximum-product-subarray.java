3f45b8d44676f7646ea2bdb53d58540384466988
Aniket Baidya <aniketbaidyarocks@gmail.com>
2026-04-09 11:48:16 +0100

    Time: 2 ms (69.16%), Space: 47.5 MB (21.06%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,36 +1,19 @@
-class Solution 
-{
-    public int maxProduct(int[] nums) 
-    {
-        int n=nums.length;
-        int max_product1=Integer.MIN_VALUE;
-        int product_so_far=1;
-        int max_product2=Integer.MIN_VALUE;
-        for(int i=0;i<n;i++)
-        {
-            product_so_far*=nums[i];
-            if(product_so_far>max_product1)
-            {
-                max_product1=product_so_far;
+class Solution {
+    public int maxProduct(int[] nums) {
+        // TRack best and worse product
+        int res = nums[0];
+        int max = nums[0];
+        int min = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] < 0) {
+                int temp = max;
+                max = min;
+                min = temp;
             }
-            if(product_so_far==0)
-            {
-                product_so_far=1;
-            }
+            max = Math.max(nums[i], nums[i] * max);
+            min = Math.min(nums[i], nums[i] * min);
+            res = Math.max(res, max);
         }
-        int product_so_far1=1;
-        for(int i=n-1;i>=0;i--)
-        {
-            product_so_far1*=nums[i];
-            if(product_so_far1>max_product2)
-            {
-                max_product2=product_so_far1;
-            }
-            if(product_so_far1==0)
-            {
-                product_so_far1=1;
-            }
-        }
-        return Math.max(max_product1,max_product2);
+        return res;
     }
 }
