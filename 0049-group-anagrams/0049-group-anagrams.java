70462b9faafdfd7d7c50ec448e46a9556f77c11b
Aniket Baidya <aniketbaidyarocks@gmail.com>
2026-04-16 16:26:32 +0100

    Time: 7 ms (74.4%), Space: 49.6 MB (63.83%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,22 +1,26 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        // Sorting Approach
-        List<List<String>> result = new ArrayList<>();
-        Map<String, List<String>> map = new HashMap<>();
-        for (int i = 0; i < strs.length; i++) {
-            String str = strs[i];
-            // Convert to char array
-            char[] chars = str.toCharArray();
-            // Sort the char array
-            Arrays.sort(chars);
-            // Convert back to string
-            String sortedStr = new String(chars);
-            if (!map.containsKey(sortedStr)) {
-                map.put(sortedStr, new ArrayList<String>());
+        // Hashing Solution
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        for (String s : strs) {
+            char[] ch = s.toCharArray();
+            Arrays.sort(ch);
+            String sorted = new String(ch);
+            if (map.containsKey(sorted)) {
+                List<String> group = map.get(sorted);
+                group.add(s);
+                map.put(sorted, group);
+            } else {
+                List<String> group = new ArrayList<>();
+                group.add(s);
+                map.put(sorted, group);
             }
-            map.get(sortedStr).add(str);
         }
-        result.addAll(map.values());
-        return result;
+        List<List<String>> ans = new ArrayList<>();
+        for (List<String> groups : map.values()) {
+            ans.add(groups);
+        }
+        return ans;
     }
 }
