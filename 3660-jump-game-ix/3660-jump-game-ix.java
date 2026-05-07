40f564b218f4daa9f8efcb908e9baf27fd8ed4bf
Aniket Baidya <aniketbaidyarocks@gmail.com>
2026-05-07 15:59:20 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3660-jump-game-ix/3660-jump-game-ix.java
+++ 3660-jump-game-ix/3660-jump-game-ix.java
@@ -1,39 +1,16 @@
 class Solution {
     public int[] maxValue(int[] nums) {
-        int n = nums.length;
+        int n = nums.length, min = Integer.MAX_VALUE;
+        int[] max = new int[n], ans = new int[n];
+        max[0] = nums[0];
+        for (int i = 1; i < n; i++)
+            max[i] = Math.max(max[i - 1], nums[i]);
 
-        int[] pre = new int[n];
-        int[] suf = new int[n];
-        int[] res = new int[n];
-
-        // prefix max
-        pre[0] = nums[0];
-        for (int i = 1; i < n; i++) {
-            pre[i] = Math.max(pre[i - 1], nums[i]);
-        }
-
-        // suffix min
-        suf[n - 1] = nums[n - 1];
         for (int i = n - 2; i >= 0; i--) {
-            suf[i] = Math.min(suf[i + 1], nums[i]);
+            min = Math.min(min, nums[i + 1]);
+            if (min < max[i])
+                max[i] = max[i + 1];
         }
-
-        res[n - 1] = pre[n - 1];
-
-        // build answer
-        for (int i = n - 2; i >= 0; i--) {
-
-            // merge segment
-            if (pre[i] > suf[i + 1]) {
-                res[i] = res[i + 1];
-            }
-
-            // new segment
-            else {
-                res[i] = pre[i];
-            }
-        }
-
-        return res;
+        return max;
     }
 }
