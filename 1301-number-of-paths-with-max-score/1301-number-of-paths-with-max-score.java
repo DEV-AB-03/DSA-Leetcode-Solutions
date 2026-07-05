7e84a2d5d4a9d475d05ab7bda20d873f1dfb699e
Aniket Baidya <aniketbaidyarocks@gmail.com>
2026-07-05 18:37:46 +0100

    Time: 9 ms (85.88%), Space: 47.1 MB (28.24%) - LeetHub

--- 1301-number-of-paths-with-max-score/1301-number-of-paths-with-max-score.java
+++ 1301-number-of-paths-with-max-score/1301-number-of-paths-with-max-score.java
@@ -1,47 +1,102 @@
 class Solution {
+    int MOD = 1_000_000_007;
+    int[][] dirs = { { -1, 0 }, { 0, -1 }, { -1, -1 } };
 
-    private final int MOD = 1000000007;
-
-    public int[] pathsWithMaxScore(List<String> board) {
-        int n = board.size();
-        int[][][] dp = new int[n][n][2];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                dp[i][j][0] = -1;
-            }
-        }
-        dp[n - 1][n - 1][0] = 0;
-        dp[n - 1][n - 1][1] = 1;
-
-        for (int i = n - 1; i >= 0; i--) {
-            for (int j = n - 1; j >= 0; j--) {
-                if (!(i == n - 1 && j == n - 1) && board.get(i).charAt(j) != 'X') {
-                    update(dp, i, j, i + 1, j, n);
-                    update(dp, i, j, i, j + 1, n);
-                    update(dp, i, j, i + 1, j + 1, n);
-                    if (dp[i][j][0] != -1) {
-                        dp[i][j][0] += board.get(i).charAt(j) == 'E'
-                                ? 0
-                                : board.get(i).charAt(j) - '0';
-                    }
-                }
-            }
-        }
-        if (dp[0][0][0] != -1) {
-            return new int[] { dp[0][0][0], dp[0][0][1] % MOD };
-        }
-        return new int[] { 0, 0 };
+    public boolean outOfBounds(int i, int j, int n) {
+        return i < 0 || i >= n || j < 0 || j >= n;
     }
 
-    private void update(int[][][] dp, int x, int y, int u, int v, int n) {
-        if (u >= n || v >= n || dp[u][v][0] == -1) {
-            return;
+    public int countPaths(int i, int j, char[][] grid, int sum, int[][] pathDp, int[][] dp) {
+        // Base Case
+        if (i == 0 && j == 0 && sum == 0) {
+            // Found a valid path
+            return 1;
         }
-        if (dp[u][v][0] > dp[x][y][0]) {
-            dp[x][y][0] = dp[u][v][0];
-            dp[x][y][1] = dp[u][v][1];
-        } else if (dp[u][v][0] == dp[x][y][0]) {
-            dp[x][y][1] = (dp[x][y][1] + dp[u][v][1]) % MOD;
+        if (outOfBounds(i, j, grid.length) || grid[i][j] == 'X' || sum < 0 || sum != dp[i][j]) {
+            return 0;
         }
+        if (pathDp[i][j] != -1) {
+            return pathDp[i][j];
+        }
+        int count = 0;
+        for (int[] d : dirs) {
+            int ni = i + d[0];
+            int nj = j + d[1];
+
+            int childResult = countPaths(ni, nj, grid, sum - Integer.parseInt(String.valueOf(grid[i][j])), pathDp, dp);
+            count = (count + childResult) % MOD;
+        }
+
+        // Return answer to Parent
+        return pathDp[i][j] = count;
+
+    }
+
+    public int dfs(int i, int j, char[][] grid, int[][] dp) {
+        //Base Case
+        if (i == 0 && j == 0) {
+            return 0;
+        }
+        if (outOfBounds(i, j, grid.length) || grid[i][j] == 'X') {
+            return Integer.MIN_VALUE;
+        }
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+        int res = Integer.MIN_VALUE;
+        for (int[] d : dirs) {
+            int ni = i + d[0];
+            int nj = j + d[1];
+
+            int childResult = dfs(ni, nj, grid, dp);
+            if (childResult == Integer.MIN_VALUE) {
+                continue;
+            }
+            res = Math.max(res, Integer.parseInt(String.valueOf(grid[i][j])) + childResult);
+        }
+
+        // Return answer to parent
+        return dp[i][j] = res;
+
+    }
+
+    private char[][] buildMatrix(List<String> board) {
+        int n = board.size();
+        char[][] grid = new char[n][n];
+        for (int i = 0; i < n; i++) {
+            grid[i] = board.get(i).toCharArray();
+        }
+        // Mark S and E as 0
+        grid[n - 1][n - 1] = '0';
+        grid[0][0] = '0';
+        return grid;
+    }
+
+    public int[] pathsWithMaxScore(List<String> board) {
+        // Recursion
+        char[][] grid = buildMatrix(board);
+        int n = grid.length;
+
+        // memoization
+        int[][] dp = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+        int maxSum = dfs(n - 1, n - 1, grid, dp);
+
+        if (maxSum == Integer.MIN_VALUE) {
+            return new int[] { 0, 0 };
+        }
+
+        // memoization
+        int[][] pathDp = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(pathDp[i], -1);
+        }
+
+        int numPaths = countPaths(n - 1, n - 1, grid, maxSum, pathDp, dp);
+        numPaths = numPaths % MOD;
+
+        return new int[] { maxSum, numPaths };
     }
 }
