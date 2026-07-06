95fd08d723abcfa31f9dc2650b07c7ce0a63cf65
Aniket Baidya <aniketbaidyarocks@gmail.com>
2026-07-06 15:32:33 +0100

    Time: 2 ms (88.85%), Space: 47.8 MB (62.53%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,25 +1,16 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        // First Observation: If All integers in the array are greater than zero then we can definitely reach end no matter what, so we will check this case first
-        boolean allNumsGreatThanZero = true;
-        for (int num : nums) {
-            if (num == 0) {
-                allNumsGreatThanZero = false;
-                break;
-            }
-        }
-        if (allNumsGreatThanZero) {
-            return true;
-        }
-
-        // Second Observation: At each index we will try to maintain a variable which will denote the maximum Index i can jump to from that position. If at certain case, we find that maxIdx>=arr.length-1, then we can reach end and stop there only.
-        int maxIdx = 0;
+        int maxPos = 0;
         for (int i = 0; i < nums.length; i++) {
-            maxIdx = Math.max(maxIdx, i + nums[i]);
-            if (maxIdx >= nums.length - 1) {
+            if (maxPos < i) {
+                // Cant reach the current index
+                return false;
+            }
+            maxPos = Math.max(maxPos, i + nums[i]);
+            if (maxPos >= nums.length) {
                 return true;
             }
         }
-        return false;
+        return true;
     }
 }
