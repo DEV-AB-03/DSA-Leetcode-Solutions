efb6f7b1d12066eb4c84da32da948dbefd6bc0d1
Aniket Baidya <aniketbaidyarocks@gmail.com>
2026-07-06 12:48:46 +0100

    Time: 1 ms (99.96%), Space: 80.7 MB (7.78%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        // Kadane's Algorithm
+        // Greedy
+        int sum = Integer.MIN_VALUE;
+        int currSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            currSum += nums[i];
+            sum = Math.max(sum, currSum);
+            if (currSum < 0) {
+                currSum = 0;
+            }
+        }
+        return sum;
+    }
+}
